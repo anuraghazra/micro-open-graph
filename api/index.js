0fb33e8c40c5de6f68740e767adee5f450423155
anuraghazra <hazru.anurag@gmail.com>
2020-07-05 18:48:53 +0530

    chore: remove micro

--- api/index.js
+++ api/index.js
@@ -1,23 +1,37 @@
 const { parse } = require('url')
-const { send } = require('micro')
 const got = require('got');
 const cache = require('memory-cache')
 
+const metascraperAuthor = require('metascraper-author');
+const metascraperDate = require('metascraper-date');
+const metascraperDescription = require('metascraper-description');
+const metascraperImage = require('metascraper-image');
+const metascraperLogo = require('metascraper-logo');
+const metascraperClearbit = require('metascraper-clearbit-logo');
+const metascraperLogo = require('metascraper-logo-favicon');
+const metascraperPublisher = require('metascraper-publisher');
+const metascraperTitle = require('metascraper-title');
+const metascraperUrl = require('metascraper-url');
+const metascraperLogo = require('metascraper-logo-favicon');
+const metascraperAmazon = require('metascraper-amazon');
+const metascraperYoutube = require('metascraper-youtube');
+const metascraperSoundcloud = require('metascraper-soundcloud');
+
 const metascraper = require('metascraper').load([
-  require('metascraper-author')(),
-  require('metascraper-date')(),
-  require('metascraper-description')(),
-  require('metascraper-image')(),
-  require('metascraper-logo')(),
-  require('metascraper-clearbit-logo')(),
-  require('metascraper-logo-favicon')(),
-  require('metascraper-publisher')(),
-  require('metascraper-title')(),
-	require('metascraper-url')(),
-	require('metascraper-logo-favicon')(),
-	require('metascraper-amazon')(),
-	require('metascraper-youtube')(),
-	require('metascraper-soundcloud')(),
+  metascraperAuthor(),
+  metascraperDate(),
+  metascraperDescription(),
+  metascraperImage(),
+  metascraperLogo(),
+  metascraperClearbit(),
+  metascraperLogo(),
+  metascraperPublisher(),
+  metascraperTitle(),
+  metascraperUrl(),
+  metascraperLogo(),
+  metascraperAmazon(),
+  metascraperYoutube(),
+  metascraperSoundcloud()
 ])
 
 
@@ -27,10 +41,10 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
 
   const { query: { url } } = parse(req.url, true)
-  if (!url) return send(res, 401, { message: 'Please supply an URL to be scraped in the url query parameter.' })
+  if (!url) return res.status(400).send({ message: 'Please supply an URL to be scraped in the url query parameter.' })
 
   const cachedResult = cache.get(url)
-  if (cachedResult) return send(res, 200, cachedResult)
+  if (cachedResult) return res.status(200).send(cachedResult)
 
   let statusCode, data
   try {
@@ -43,7 +57,7 @@ module.exports = async (req, res) => {
     data = { message: `Scraping the open graph data from "${url}" failed.`, suggestion: 'Make sure your URL is correct and the webpage has open graph data, meta tags or twitter card data.' }
   }
 
-  send(res, statusCode, data)
+  res.status(statusCode).send(data)
   // Cache results for 24 hours
   cache.put(url, data, TWENTY_FOUR_HOURS)
 }
