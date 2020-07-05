45a7de2b30316eb35d76b0a9c4d222c400ffa3d4
anuraghazra <hazru.anurag@gmail.com>
2020-07-05 19:10:51 +0530

    fix: update scraper

--- api/index.js
+++ api/index.js
@@ -2,34 +2,21 @@ const { parse } = require('url')
 const got = require('got');
 const cache = require('memory-cache')
 
-const metascraperAuthor = require('metascraper-author');
-const metascraperDate = require('metascraper-date');
-const metascraperDescription = require('metascraper-description');
-const metascraperImage = require('metascraper-image');
-const metascraperLogo = require('metascraper-logo');
-const metascraperClearbit = require('metascraper-clearbit-logo');
-const metascraperLogoFavicon = require('metascraper-logo-favicon');
-const metascraperPublisher = require('metascraper-publisher');
-const metascraperTitle = require('metascraper-title');
-const metascraperUrl = require('metascraper-url');
-const metascraperAmazon = require('metascraper-amazon');
-const metascraperYoutube = require('metascraper-youtube');
-const metascraperSoundcloud = require('metascraper-soundcloud');
-
-const metascraper = require('metascraper').load([
-  metascraperAuthor(),
-  metascraperDate(),
-  metascraperDescription(),
-  metascraperImage(),
-  metascraperLogo(),
-  metascraperClearbit(),
-  metascraperLogoFavicon(),
-  metascraperPublisher(),
-  metascraperTitle(),
-  metascraperUrl(),
-  metascraperAmazon(),
-  metascraperYoutube(),
-  metascraperSoundcloud()
+const metascraper = require('metascraper')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-image')(),
+  require('metascraper-logo')(),
+  require('metascraper-clearbit-logo')(),
+  require('metascraper-logo-favicon')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+	require('metascraper-url')(),
+	require('metascraper-logo-favicon')(),
+	require('metascraper-amazon')(),
+	require('metascraper-youtube')(),
+	require('metascraper-soundcloud')(),
 ])
 
 
@@ -39,7 +26,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
 
   const { query: { url } } = parse(req.url, true)
-  if (!url) return res.status(400).send({ message: 'Please supply an URL to be scraped in the url query parameter.' })
+  if (!url) return req.status(400).send({ message: 'Please supply an URL to be scraped in the url query parameter.' })
 
   const cachedResult = cache.get(url)
   if (cachedResult) return res.status(200).send(cachedResult)
@@ -55,7 +42,7 @@ module.exports = async (req, res) => {
     data = { message: `Scraping the open graph data from "${url}" failed.`, suggestion: 'Make sure your URL is correct and the webpage has open graph data, meta tags or twitter card data.' }
   }
 
-  res.status(statusCode).send(data)
+  req.status(statusCode).send(data)
   // Cache results for 24 hours
   cache.put(url, data, TWENTY_FOUR_HOURS)
 }
